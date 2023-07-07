5a85dfe0ba2c00fbeb3b68614fd6533a3f659769
Mateusz Wróbel <mw.animacja@gmail.com>
2023-07-07 11:11:33 +0200

    add more listItem for Menu, change style of title in Home component

--- src/helpers/Menu.lst.jsx
+++ src/helpers/Menu.lst.jsx
@@ -4,32 +4,59 @@ import Neapolitana from '../assets/Neapolitana.jpg';
 import VeganPizza from '../assets/Vegan.jpg';
 import Havana from '../assets/Tono.jpg';
 import PizzaEspeciale from '../assets/Especiale.jpg';
+import CheeseePizza from '../assets/CheeseePizza.jpg';
+import DonMateo from '../assets/DonMateo.jpg';
+import Hell from '../assets/Hell.jpg';
+import Primavera from '../assets/Primavera.jpg'
 
 
 export const MenuList =[
     {
         name: 'Pepperoni Pizza',
         image: Pepperoni,
-        price: 23.99
+        price: `13.99 €`
     },
     {
         name: 'Neapolitana',
         image: Neapolitana,
-        price: 15.00,
+        price: `15.00 €`
     },
     {
         name: 'Vegan Pizza',
         image: VeganPizza,
-        price: 24.54
+        price: `8.54 €`
     },
     {
         name: 'Havana',
         image: Havana,
-        price: 28
+        price: `12 €`
     },
     {
         name: 'Pizza Especiale',
         image: PizzaEspeciale,
-        price: 30.99
+        price: `30.99 €`
+    },
+    {
+        name: 'Pizza Don Mateo',
+        image: DonMateo,
+        price: `20.99 €`
+    },
+
+    {
+        name: 'Cheesee Pizza',
+        image: CheeseePizza,
+        price: `10.99 €`
+    },
+
+    {
+        name: 'Primavera',
+        image: Primavera,
+        price: `8.99 €`
+    },
+
+    {
+        name: 'Hell',
+        image: Hell,
+        price: `17.99 €`
     }
 ]
